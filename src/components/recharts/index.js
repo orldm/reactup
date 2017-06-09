b39d7f19d58c7676284546044bb77c0184c47771
Dmytro Orlov <orldm@mail.ru>
2017-06-09 18:10:23 +0300

    add more recharts examples

--- src/components/recharts/index.js
+++ src/components/recharts/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { LineChart, AreaChart, PieChart, Pie, Area, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 const data = [
       {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
@@ -11,9 +11,27 @@ const data = [
       {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
 ]
 
+const data01 = [
+  { name: 'Group A', value: 400, v: 89 },
+  { name: 'Group B', value: 300, v: 100 },
+  { name: 'Group C', value: 200, v: 200 },
+  { name: 'Group D', value: 200, v: 20 },
+  { name: 'Group E', value: 278, v: 40 },
+  { name: 'Group F', value: 189, v: 60 },
+]
+
+const data02 = [
+  { name: 'Group A', value: 2400 },
+  { name: 'Group B', value: 4567 },
+  { name: 'Group C', value: 1398 },
+  { name: 'Group D', value: 9800 },
+  { name: 'Group E', value: 3908 },
+  { name: 'Group F', value: 4800 },
+]
+
 const Recharts = () => (
-  <div className='align-center'>
-    <ResponsiveContainer width='100%' height={400} >
+  <div className="flex-container">
+    <ResponsiveContainer width='50%' height={400} >
       <LineChart data={data}
               margin={{top: 5, right: 30, left: 20, bottom: 5}}>
           <XAxis dataKey="name"/>
@@ -24,6 +42,33 @@ const Recharts = () => (
           <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
           <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
       </LineChart>
+    </ResponsiveContainer>
+    <ResponsiveContainer width='50%' height={400} >
+        <AreaChart data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+            <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                </linearGradient>
+            </defs>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+        </AreaChart>
+    </ResponsiveContainer>
+    <ResponsiveContainer width='100%' height={400} >
+        <PieChart>
+            <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" label />
+            <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={110} outerRadius={150} fill="#82ca9d" label />
+        </PieChart>
     </ResponsiveContainer>
   </div>
 )
