ee27f50b3cd0351257b43d67e41528c8fb41c838
Dmytro Orlov <orldm@mail.ru>
2017-06-14 14:20:27 +0300

    update folder structure

--- src/App.js
+++ src/App.js
@@ -7,28 +7,21 @@ import {
 import ReactVis from './components/react-vis'
 import Recharts from './components/recharts'
 
-import logo from './logo.svg'
-import './App.css'
+// import logo from './logo.svg'
+import './styles/App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        {/*<div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome</h2>
-        </div>
+        </div>*/}
         <Router>
           <div>
-            <ul className='link-container'>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/recharts">Recharts</Link></li>
-              <li><Link to="/react-vis">React-Vis</Link></li>
-            </ul>
-
-            <hr/>
-
             <Route exact path="/" component={Home}/>
+            <Route path="/in-depth" component={InDepth}/>
             <Route path="/recharts" component={Recharts}/>
             <Route path="/react-vis" component={ReactVis}/>
           </div>
@@ -40,7 +33,17 @@ class App extends Component {
 
 const Home = () => (
   <div>
-    <p>Our website is coming soon :-)</p>
+    <ul className='link-container'>
+      <li><Link to="/in-depth">In-Depth</Link></li>
+      <li><Link to="/recharts">Recharts</Link></li>
+      <li><Link to="/react-vis">React-Vis</Link></li>
+    </ul>
+  </div>
+)
+
+const InDepth = () => (
+  <div>
+    <h1>In-Depth Dashboard</h1>
   </div>
 )
 
